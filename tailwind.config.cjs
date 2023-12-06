8beb275ab16e44d8ab8b93cefaddd6f1c4140d82
zaida.rivai@gmail.com <zaida.rivai@gmail.com>
2023-12-06 19:59:01 +0100

    finetune

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
         accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
+        greygreen:'#344B47',
       },
       fontFamily: {
         sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
